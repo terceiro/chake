7d25cc2dd6cc0dad431e421aef5e580e56beb7d9
Antonio Terceiro <terceiro@softwarelivre.org>
2015-02-06 15:49:55 -0200

    Fix Debian bootstrap script

--- lib/chake/bootstrap/00_debian.sh
+++ lib/chake/bootstrap/00_debian.sh
@@ -1,6 +1,7 @@
 if [ -x /usr/bin/apt-get ]; then
   apt-get update
-  export DEBIAN_FRONTEND=noninteractive apt-get -q -y install rsync chef
+  export DEBIAN_FRONTEND=noninteractive
+  apt-get -q -y install rsync chef
   update-rc.d chef-client disable
   service chef-client stop
   exit
