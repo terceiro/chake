4b2a1ba0540043a8910b48ac22f904da58268715
Antonio Terceiro <terceiro@softwarelivre.org>
2015-06-04 15:06:05 -0300

    Stop cloud-init from resetting the hostname

--- lib/chake/bootstrap/00_set_hostname.sh
+++ lib/chake/bootstrap/00_set_hostname.sh
@@ -2,3 +2,8 @@ hostname="$1"
 
 echo "$hostname" > /etc/hostname
 hostname --file /etc/hostname
+
+# Stop cloud-init from resetting the hostname
+if [ -f /etc/cloud/cloud.cfg ]; then
+  sed -i -e '/^\s*-\s*\(set_hostname\|update_hostname\)/d' /etc/cloud/cloud.cfg
+fi
