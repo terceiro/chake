b0619213ae4cf882734989ab67a6ee5bb308ab3d
Antonio Terceiro <terceiro@softwarelivre.org>
2015-10-28 18:47:48 -0200

    bootstrap: make sure FQDN matches hostname
    This is crucial for cookbooks/*/{files,templates}/host-*/ to work properly.

--- lib/chake/bootstrap/00_set_hostname.sh
+++ lib/chake/bootstrap/00_set_hostname.sh
@@ -3,6 +3,11 @@ hostname="$1"
 echo "$hostname" > /etc/hostname
 hostname --file /etc/hostname
 
+fqdn=$(hostname --fqdn || true)
+if [ "$fqdn" != "$hostname" ]; then
+  printf "127.0.1.1\t%s\n" "$hostname" >> /etc/hosts
+fi
+
 # Stop cloud-init from resetting the hostname
 if [ -f /etc/cloud/cloud.cfg ]; then
   sed -i -e '/^\s*-\s*\(set_hostname\|update_hostname\)/d' /etc/cloud/cloud.cfg
