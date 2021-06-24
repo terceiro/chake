a12da25119f1d7bd1c55b7ab7270fb92b657b797
Antonio Terceiro <asa@terceiro.xyz>
2021-06-24 17:43:53 -0300

    chake/config: store the node definition file in the node data

--- lib/chake/config.rb
+++ lib/chake/config.rb
@@ -8,9 +8,16 @@ end
 
 nodes_file = ENV['CHAKE_NODES'] || 'nodes.yaml'
 nodes_directory = ENV['CHAKE_NODES_D'] || 'nodes.d'
-node_data = File.exist?(nodes_file) && YAML.load_file(nodes_file) || {}
+nodes = File.exist?(nodes_file) && YAML.load_file(nodes_file) || {}
+nodes.values.each do |node|
+  node['chake_metadata'] = { "definition_file": nodes_file }
+end
 Dir.glob(File.join(nodes_directory, '*.yaml')).sort.each do |f|
-  node_data.merge!(YAML.load_file(f))
+  file_nodes = YAML.load_file(f)
+  file_nodes.values.each do |node|
+    node['chake_metadata'] = { "definition_file": f }
+  end
+  nodes.merge!(file_nodes)
 end
 
-Chake.nodes = node_data.map { |node, data| Chake::Node.new(node, data) }.reject(&:skip?).uniq(&:hostname)
+Chake.nodes = nodes.map { |node, data| Chake::Node.new(node, data) }.reject(&:skip?).uniq(&:hostname)
